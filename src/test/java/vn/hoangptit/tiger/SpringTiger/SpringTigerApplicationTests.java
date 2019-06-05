9a9669e6d2da736c110e099bebdca4d53cacbbd8
hoangtd5 <duyhoangptit@gmail.com>
2019-06-05 22:45:30 +0700

    add project

---
+++ src/test/java/vn/hoangptit/tiger/SpringTiger/SpringTigerApplicationTests.java
@@ -0,0 +1,16 @@
+package vn.hoangptit.tiger.SpringTiger;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringTigerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
